509c565803c610b4e8fbfb1abe7e5c2ca50b41fa
Rishi Pardeshi <rishipardeshi567+github@gmail.com>
2025-07-20 16:09:15 +0530

    Authentication

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Link } from "react-router";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <Link to={"/"} className="text-2xl font-bold text-gradient">
+        <p>RESUMIND</p>
+      </Link>
+      <Link to={"/upload"} className="primary-button w-fit">
+        Upload Resume{" "}
+      </Link>
+    </nav>
+  );
+};
+
+export default Navbar;
